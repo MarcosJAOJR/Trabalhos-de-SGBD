5e474be154c2a2e390beda3bbc6704226d288ed1
Marcos Augusto <mjaojr@gmail.com>
2016-06-01 23:17:02 -0300

    feat(Schedule): Create parse method's interface

---
+++ src/trabalho3/Schedule.java
@@ -0,0 +1,21 @@
+package trabalho3;
+
+import java.util.LinkedList;
+
+import trabalho2.Evento;
+
+public class Schedule {
+	
+	LinkedList<Evento> operations;
+	
+	Schedule(String schedule) {
+		this.parser(schedule); 				
+	}
+	
+	public boolean parser(String schedule) {
+		
+		// TODO: Converter uma string em uma fila de comandos e salvar essa fila no this.operations
+		
+		return true;
+	}
+}
